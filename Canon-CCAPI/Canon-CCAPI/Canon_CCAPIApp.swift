8c1d0a0104437cdf37dc34e760543ffa6117981d
realhsb <realhsb@gmail.com>
2025-09-22 19:49:03 +0900

    init project

---
+++ Canon-CCAPI/Canon-CCAPI/Canon_CCAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Canon_CCAPIApp.swift
+//  Canon-CCAPI
+//
+//  Created by Subeen on 9/22/25.
+//
+
+import SwiftUI
+
+@main
+struct Canon_CCAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Canon-CCAPI/Canon-CCAPI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Canon-CCAPI
+//
+//  Created by Subeen on 9/22/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
