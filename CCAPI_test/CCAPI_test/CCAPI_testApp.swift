fb8588a6afce202b0589360068c29f1a4495ba32
realhsb <realhsb@gmail.com>
2025-09-29 23:31:28 +0900

    chore: 시작 지점 변경

--- CCAPI_test/CCAPI_test/CCAPI_testApp.swift
+++ CCAPI_test/CCAPI_test/CCAPI_testApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct CanonCameraApp: App {
     var body: some Scene {
         WindowGroup {
-            CameraDiscoveryView()
+            CameraDirectControlView()
         }
     }
 }
